35b73a5ea9fc38b40eb3e9c617ed80e57c7e58b1
Paul <paul.delarue@student.junia.com>
2022-04-25 16:07:02 +0200

    change main script name

--- main.py
+++ main.py
@@ -1,82 +1,57 @@
-import tensorflow.keras as tfk
-from matplotlib import pyplot as plt
-from tensorflow.keras.datasets import mnist
-from tensorflow.python.keras.callbacks import ModelCheckpoint
-from tensorflow.python.keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
-from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
-from tensorflow.python.keras.models import Sequential, load_model
-# Loading de MNIST data
-(X_train, y_train), (X_valid, y_valid) = mnist.load_data()
+import numpy as np
+import cv2
+import argparse
 
-print(X_train.shape)
-print(y_train.shape)
-print(y_train[0:12])
+from Segmentation import lecture_img, getSegmentedSudoku
+from MakeSudokuMatrice import MakeSudokuMatrice
+from SudokuSolver.backtracking_csp_algorithm import backtracking_csp_algorithm
+from getSudokuGridFromImage import getSudokuGridFromImage
+from SudokuSolver.View import View
+from checkNumber import checkSudoku
 
-plt.figure(figsize=(5, 5))
-for k in range(12):
-    plt.subplot(3, 4, k + 1)
-    plt.imshow(X_train[k], cmap="Greys")
-    plt.axis("off")
-plt.tight_layout()
-plt.show()
-X_train = X_train.reshape(60000, 784).astype("float32")
-X_valid = X_valid.reshape(10000, 784).astype("float32")
-X_train /= 255
-X_valid /= 255
-n_classes = 10
-y_train = tfk.utils.to_categorical(y_train, n_classes)
-y_valid = tfk.utils.to_categorical(y_valid, n_classes)
 
-model = Sequential()
+def main():
+    # We first create the ArgumentParser object
+    # The created object 'parser' will have the necessary information
+    # to parse the command-line arguments into data types.
+    parser = argparse.ArgumentParser()
 
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(120, 120, 3)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(256, (3, 3), activation='relu'))
-model.add(Dropout(0.2))
+    # Add 'path_image_input' argument using add_argument() including a help. The type is string (by default):
+    parser.add_argument("path_image_input", help="path to input image to be displayed")
 
-model.add(Flatten())
+    # Parse the argument and store it in a dictionary:
+    args = vars(parser.parse_args())
 
-model.add(Dense(512, activation='relu'))
+    INPUT_PATH = args["path_image_input"]
+    # Parameter to put in Config : "images/raw/realSudoku.jpg"
+    #image = lecture_img("images/raw/sudoku.png")
+    image = lecture_img(INPUT_PATH)
+    image = cv2.resize(image, (450,450))
+    cv2.imshow("img", image)
+    cv2.waitKey(0)
+    grid = getSudokuGridFromImage(image)
+    cv2.imshow("grid", grid)
+    cv2.waitKey(0)
+    segmentedSudoku = getSegmentedSudoku(grid)
+    #segmentedSudoku = getSegmentedSudoku(image)
+    print(len(segmentedSudoku))
+    sudoku = MakeSudokuMatrice(segmentedSudoku)
+    #make sudoku a numpy array
+    npSudoku = np.array(sudoku)
+    print("get sudoku :")
+    print(npSudoku)
+    print("solve sudoku :")
 
-model.add(Dense(len(X_train), activation='softmax'))
+    npSudoku = checkSudoku(npSudoku)
 
-print(model.summary())
+    canBeSolved, solvedSudoku = backtracking_csp_algorithm(npSudoku)
 
-checkpoint = ModelCheckpoint("custom_model", save_best_only=True)
-model.compile(optimizer=SGD(learning_rate=0.01),
-              loss='categorical_crossentropy',
-              metrics=['acc'])
+    # If it cannot be solved :
+    if not canBeSolved:
+        print("This sudoku is insolvable")
+    else:
+        # Display the solution
+        View(solvedSudoku)
 
-# history = model.fit(train_generator,validation_data = validation_generator,epochs = 20, verbose = 1, callbacks = [checkpoint])
-model = load_model("custom_model")
-
-#acc = history.history['acc']
-#val_acc = history.history['val_acc']
-#loss = history.history['loss']
-#val_loss = history.history['val_loss']
-
-#epochs = range(len(acc))
-
-plt.figure(figsize=(6, 5))
-
-plt.plot(epochs, acc, 'r', label='training_accuracy')
-plt.plot(epochs, val_acc, 'b', label='validation_accuracy')
-plt.title('Training and Validation Accuracy')
-plt.xlabel('epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-
-plt.figure(figsize=(6, 5))
-
-plt.plot(epochs, loss, 'r', label='training_loss')
-plt.plot(epochs, val_loss, 'b', label='validation_loss')
-plt.title('Training and Validation Loss')
-plt.xlabel('epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.show()
+if __name__ == "__main__" :
+    main()
